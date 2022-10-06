0aa99b396bb1e75ce43df6620878d72145e24c3a
Sarahpixel <107088382+Sarahpixel@users.noreply.github.com>
2022-10-06 16:11:17 +1100

    Laser

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class MouseLook : MonoBehaviour
 {
 
-    public float mouseSensitivity = 100f;
+    public float mouseSensitivity = 2000f;
     public Transform playerBody;
     float xRotation = 0f;
 
