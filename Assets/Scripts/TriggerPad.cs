676be8354732d04133665b1c943a48ff2fb79b38
Sarahpixel <107088382+Sarahpixel@users.noreply.github.com>
2022-10-18 18:16:38 +1100

    Triggerpad
    some functions seem to not work

--- Assets/Scripts/TriggerPad.cs
+++ Assets/Scripts/TriggerPad.cs
@@ -4,34 +4,21 @@ using UnityEngine;
 
 public class TriggerPad : MonoBehaviour
 {
-    public GameObject sphere;   //The object we wish to change
+    [SerializeField] private Renderer myObject;
 
-    void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Player"))
+        if (CompareTag("Player"))
         {
-            //change the spheres colour to green
-            sphere.GetComponent<Renderer>().material.color = Color.green;
+           myObject.material.color = Color.green;
         }
     }
-
-    void OnTriggerStay(Collider other)
+    private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Player"))
-        {
-            //Increases the spheres scale by 0.01 on all axis
-            sphere.transform.localScale = Vector3.one * 0.01f;
-        }
-    }
 
-    void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player"))
+        if (CompareTag("Player"))
         {
-            //set the spheres size back to 1
-            sphere.transform.localScale = Vector3.one;
-            //Change the spheres colour to yellow
-            sphere.GetComponent<Renderer>().material.color = Color.yellow;
+            myObject.material.color = Color.blue;
         }
     }
 }
